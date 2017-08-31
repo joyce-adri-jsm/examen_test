2a3a6446edb673d84da06afe03ee174bb1b02eec
joyce-adri-jsm <joyce01sm@hotmail.com>
2017-08-31 05:45:00 -0500

    prueba de coveralls 2

--- Version_A/test.py
+++ Version_A/test.py
@@ -2,12 +2,19 @@
 from __future__ import unicode_literals
 
 import unittest
+
 import caso1
+import caso2
+
 
 class Test(unittest.TestCase):
-    def test1(self):
+    def test_caso1(self):
         resultado = caso1.etiquetado_consumo_energia(25)
         self.assertEquals(resultado, "escala: A++ , eficiencia: los más eficientes")
+
+    def test_caso2(self):
+        resultado = caso2.clasificacion_clientes(45000 ,True)
+        self.assertEquals(resultado, "Tipo de cliente: AAA, Envio de boletin: Si")
 
 if __name__ == '__main__':
     unittest.main()
